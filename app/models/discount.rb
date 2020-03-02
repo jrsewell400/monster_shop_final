139f9d98ab519c7ffdfb1d9501a009e65387a108
Jordan Sewell <jrsewell400@gmail.com>
2020-03-01 18:33:14 -0700

    Add uniqueness and inclusion validations to protect against edge cases of duplicate discount names and percentage amounts as well as percentage and quantity ranges

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,9 +1,15 @@
 class Discount < ApplicationRecord
   belongs_to :merchant
 
+
   validates_presence_of :name,
                         :description,
                         :discount_amt,
                         :req_qty
 
+  validates_uniqueness_of :name, 
+                          :discount_amt
+
+  validates_inclusion_of :discount_amt, in: 0.001..0.99, message: "Discount must be between 0.001 and .99"
+  validates_inclusion_of :req_qty, in: 0..999, message: "Discount must be between 0.001 and .99"
 end
