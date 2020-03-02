127f8aa0c67c40d3c98b801e1e4bd99d8408da34
Jordan Sewell <jrsewell400@gmail.com>
2020-03-02 07:14:09 -0700

    Update validation message for required_qty

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -11,5 +11,5 @@ class Discount < ApplicationRecord
                           :discount_amt
 
   validates_inclusion_of :discount_amt, in: 0.001..0.99, message: "Discount must be between 0.001 and .99"
-  validates_inclusion_of :req_qty, in: 0..999, message: "Discount must be between 0.001 and .99"
+  validates_inclusion_of :req_qty, in: 0..999, message: "Discount must be between 0 and 999"
 end
