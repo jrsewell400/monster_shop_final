b8364378a90fdbbe25bbcc1ef873049ef5cac792
Jordan Sewell <jrsewell400@gmail.com>
2020-03-01 18:32:22 -0700

    Add new and create methods  as well as private params and helper method for returning true if a field is empty

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,30 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show 
     @discount = Discount.find(params[:id])
   end
+
+  def new
+    @discount = Discount.new
+  end
+
+  def create
+    @merchant = current_user.merchant
+    @discount = @merchant.discounts.new(discount_params)
+    if @discount.save
+      redirect_to merchant_discounts_path
+    else
+      generate_flash(@discount)
+      render :new
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :description, :discount_amt, :req_qty)
+  end
+
+  def field_empty?
+    params = discount_params
+    params[:name].empty? || params[:description].empty? || params[:discount_amt].empty? || params[:req_qty].empty?
+  end
 end 
